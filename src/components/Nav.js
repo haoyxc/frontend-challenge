3ee89639a4c5897be46e652a16e5eeb44fbd2e5b
haoyxc <48307028+haoyxc@users.noreply.github.com>
2019-09-22 22:43:01 -0400

    navbar stays at top

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,23 +4,21 @@ export default function Nav({ toggleCart, inCart }) {
   let [showCart, setShowCart] = useState(false);
 
   return (
-    <div>
-      <nav className="navbar">
-        <div className="navbar-center">
-          <span className="nav-icon">
-            <i className="fa fa-bars"></i>
-          </span>
-          <div className="logo-container">
-            <h3 className="navTitle">Penn Course Cart</h3>
-          </div>
-          <div className="cart-btn" onClick={toggleCart}>
-            <span className="nav-icon">
-              <i className="fa fa-cart-plus"></i>
-            </span>
-            <div className="cart-items">{inCart.length}</div>
-          </div>
+    <nav className="navbar">
+      <div className="navbar-center">
+        <span className="nav-icon">
+          <i className="fa fa-bars"></i>
+        </span>
+        <div className="logo-container">
+          <h3 className="navTitle">Penn Course Cart</h3>
         </div>
-      </nav>
-    </div>
+        <div className="cart-btn" onClick={toggleCart}>
+          <span className="nav-icon">
+            <i className="fa fa-cart-plus"></i>
+          </span>
+          <div className="cart-items">{inCart.length}</div>
+        </div>
+      </div>
+    </nav>
   );
 }
